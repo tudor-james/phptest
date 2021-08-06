8961950563562e443f2bd178de1a3e19fb04094f
tudor-james <github@tudorjames.com>
2021-08-06 11:28:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ try {
     $username = 'phptest';
     $password = 'cerebus';
     $dbName = 'test';
-    $dbHost = "172.16.0.4";
+    $dbHost = "172.20.0.5";
 
     // Connect using TCP
     $dsn = "mysql:host=$dbHost;dbname=$dbName";
